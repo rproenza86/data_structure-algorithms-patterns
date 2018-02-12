fbdd0b48c7e606f58a56dfb7c3db4f64bb4be8b2
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2018-02-12 16:55:49 -0500

    feat: added binary search

--- algorithms/searching/2-binary_search.js
+++
@@ -1,7 +0,0 @@
-/**
- * 
- * A binary search is used when the items in a list are in sorted order.
- * 
- * Binary search is the more efficient algorithm, but you also have to take into account the extra time 
- * it takes to sort the data set before being able to search it for a value.
- */
