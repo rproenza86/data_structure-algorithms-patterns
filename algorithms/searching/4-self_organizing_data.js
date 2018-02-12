2ef8e6dca2214536297d798c61647b37e4ce74a2
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2018-02-11 20:05:14 -0500

    feat: started selfOrganizedData

---
+++ algorithms/searching/4-self_organizing_data.js
@@ -0,0 +1,14 @@
+/**
+ * Using Self-Organizing Data
+    The fastest successful sequential searches on unordered data occur when the data being
+    searched for is located at the beginning of the data set. 
+    
+    You can ensure that a successfully found data item will be found quickly in the future by moving it 
+    to the beginning of a data set after it has been found in a search.
+
+    The concept behind this strategy is that we can minimize search times by locating items
+    that are frequently searched for at the beginning of a data set.    
+
+    This is an example of self-organized data: data that is organized not by the programmer 
+    before the program is executed, but by the program itself while the program is running.
+ */
